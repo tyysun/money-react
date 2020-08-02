72e7f582cdc96ad0dc3ded42262a8e6bce2a6084
tyy <tyysun@outlook.com>
2020-08-02 23:36:46 +0800

    :construction: 完成菜单栏

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,63 @@
 import React from 'react';
+import { HashRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Nav from './components/Nav';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav></Nav>
+      </Wrapper>
+
+    </Router>
   );
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function NoMatch() {
+  return <h2>你访问的页面不存在</h2>;
 }
 
 export default App;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+const NavWrapper = styled.nav`
+  line-height: 24px;
+  box-shadow:  0 0 3px rgba(0,0,0,0.25);
+  > ul {
+    display: flex;
+    > li {
+      width:  33.33% ;
+      text-align: center;
+      padding: 16px 0;
+    }
+  }
+`;
+
+function Nav() {
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <Link to="/tags">标签页</Link>
+        </li>
+        <li>
+          <Link to="/money">记账页</Link>
+        </li>
+        <li>
+          <Link to="/statistics">记账页</Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  )
+}
+
+
+export default Nav
