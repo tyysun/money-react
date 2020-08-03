3b5a7e12aa8035b297da15972eb18871ee4d679b
tyy <tyysun@outlook.com>
2020-08-03 23:47:56 +0800

    :construction: 创建 Layout 组件

---
+++ src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import Nav from './Nav';
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+const Layout = (props: any) => {
+  return (
+    <Wrapper>
+      <Main>
+        <h2>
+          {props.children}
+        </h2>
+      </Main>
+      <Nav/>
+    </Wrapper>
+  );
+};
+
+export default Layout;
