bbf27b2a87d275132bec563eb17096e595445ff3
tyy <tyysun@outlook.com>
2020-08-09 23:27:55 +0800

    :sparkles: 完成功能

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, { useState } from 'react';
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
   > ul {
     display: flex;
     font-size: 24px;
@@ -23,4 +24,28 @@ const CategorySection = styled.section`
     
   }
 `;
+
+const CategorySection: React.FC = () => {
+  const [categoryList] = useState<('-' | '+')[]>(['-', '+']);
+  const [category, setCategory] = useState('-');
+  const categoryMap = {
+    '-': '支出',
+    '+': '收入'
+  };
+  return (
+    <Wrapper>
+      <ul>
+        {
+          categoryList.map(c =>
+            <li className={category === c ? 'selected' : ''}
+                onClick={() => {setCategory(c);}}>{categoryMap[c]}
+            </li>
+          )
+        }
+
+      </ul>
+    </Wrapper>
+  );
+};
+
 export { CategorySection };
